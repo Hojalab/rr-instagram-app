790d70e4681c87d6364276f608e6265f8a69213e
Jonnie Spratley <jonniespratley@gmail.com>
2013-05-24 20:21:34 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RrInstagramWebapp::Application.config.secret_token = '385ba14558d2a9abc4b2f9f680b3622743dc2b95547f448215c8c0de9b494b34e64c77b726900442c912902687a2f1e2c652ae26316cd8f4cd9e0db5592f7812'
