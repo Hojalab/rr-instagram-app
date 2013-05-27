05c4b2dcead5f8bacceb68bc212e8ee1d699a78e
Jonnie Spratley <jonniespratley@gmail.com>
2013-05-26 18:18:41 -0700

    updated tests

--- spec/requests/instagrams_spec.rb
+++ spec/requests/instagrams_spec.rb
@@ -10,12 +10,29 @@ describe "Instagram Pages" do
     end
   end
   
-  
   #Test the popular page
-  
+  describe "GET /instagram/popular" do
+    it "should display the popular page" do
+      visit '/instagram/popular'
+      page.should have_content('Instagram#popular')
+    end
+  end
+    
   #Test the recent page
-  
+  describe "GET /instagram/recent" do
+    it "should display the recent page" do
+      visit '/instagram/recent'
+      page.should have_content('Instagram#recent')
+    end
+  end
+    
   #Test the trending page
+  describe "GET /instagram/trending" do
+    it "should display the trending page" do
+      visit '/instagram/trending'
+      page.should have_content('Instagram#trending')
+    end
+  end
   
   
 end
